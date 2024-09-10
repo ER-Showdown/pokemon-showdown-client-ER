93c1f480213bf7d410729de87d5b33cbe54e2ef8
Ian Davis <thekewlstore@gmail.com>
2024-09-10 00:35:19 -0400

    WebServer reload using nodemon with appropriate file watchers (#3)
    * Initial implementation using nodemon for file watching and recomp
    
    * Working nodemon

---
+++ build_and_serve.js
@@ -0,0 +1,26 @@
+var { execSync, spawn } = require("child_process");
+
+async function pipeOutput(child) {
+	return new Promise((res, rej) => {
+		//spit stdout to screen
+		child.stdout.on("data", function (data) {
+			process.stdout.write(data.toString());
+		});
+
+		//spit stderr to screen
+		child.stderr.on("data", function (data) {
+			process.stdout.write(data.toString());
+		});
+
+		child.on("close", function (code) {
+			res(code);
+		});
+	});
+}
+
+async function main() {
+	await pipeOutput(spawn("npm", ["run", "build"], { shell: true }));
+	await pipeOutput(spawn("npx", ["http-server", "-c-1"], { shell: true }));
+}
+
+main().then(() => process.exit(0));
