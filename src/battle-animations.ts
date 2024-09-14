cec9e5a32f33a215ee1784d45ab77ea98987d315
thekewlstore <thekewlstore@gmail.com>
2024-09-13 21:58:21 -0400

    Adding status indicator for bleed

--- src/battle-animations.ts
+++ src/battle-animations.ts
@@ -2769,6 +2769,8 @@ export class PokemonSprite extends Sprite {
 			status += '<span class="par">PAR</span> ';
 		} else if (pokemon.status === 'frz') {
 			status += '<span class="frz">FRZ</span> ';
+		} else if (pokemon.status === 'bleed') {
+			status += '<span class="bleed">BLD</span>';
 		}
 		if (pokemon.terastallized) {
 			status += `<img src="${Dex.resourcePrefix}sprites/types/${encodeURIComponent(pokemon.terastallized)}.png" alt="${pokemon.terastallized}" class="pixelated" /> `;
